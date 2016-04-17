9e0dca853686a29289b3678d372b1fc8317d8450
breanna <bdevorem@nd.edu>
2016-04-17 04:26:32 -0400

    parsing through file works

--- expletive.py
+++ expletive.py
@@ -17,6 +17,7 @@
 # r#|3 {a} {ccbaH}    * rule =
 # r#|3 {b} {cca}      * rule =
 # r#|3 {c} {cc}       * rule =
+# n?#+ {baa}          * input = 
 
 import sys
 import Queue
@@ -24,3 +25,51 @@ import Queue
 tape = Queue.Queue() # 2-tag queue, turing-tape
 prog = sys.argv[1] # user's program
 # print prog
+lines = []
+
+### Open file and read in data
+try:
+	with open(prog) as input_file:
+    	for i, line in enumerate(input_file):
+			lines.append(line)
+        	
+except IOError as e:
+	print str(e)
+
+
+### Parse through program
+alph = []
+halt = []
+rules = {}
+
+for line in lines:
+	line = line.replace("}", "").split("{")
+	
+	if line[0].strip() == '@|?h':
+		for sym in line[1].replace(" ", ""):
+			alph.append(sym)
+
+	elif line[0].strip() == 'h@|+':
+		for sym in line[1].replace(" ", ""):
+			halt.append(sym)
+
+	elif line[0].strip() == 'r#|3':
+		rules[line[1].strip()] = line[2].strip()
+
+	elif line[0].strip() == 'n?#+':
+		for sym in line[1].strip():
+			tape.put(sym)
+
+### Operate on queue
+
+
+
+	
+	
+
+
+
+
+
+
+
